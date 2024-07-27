40b4e79b7595331f0b612aa986958fbb0f2c5c97
ogautherot <olivier@gautherot.net>
2024-07-28 00:40:40 +0200

    LCD fixes
    Signed-off-by: ogautherot <olivier@gautherot.net>

--- include/Lcd1602.h
+++ include/Lcd1602.h
@@ -1,3 +1,4 @@
+
 /** LCD driver
  *
  */
@@ -8,6 +9,7 @@
 #include <stdint.h>
 #include <avr/io.h>
 #include <avr/cpufunc.h>
+#include <avr/pgmspace.h> 
 
 // Board-level definitions
 #define LCD1602_CTRL_PORT PORTC
@@ -22,125 +24,79 @@
 #define LCD1602_DATA_MASK 0x0f
 #define LCD1602_DATA_GET_D7 (PORTA & 0x80)
 
-// Bus handling
-/*
-#define LCD1602_RS_SET                      \
-    do                                      \
-    {                                       \
-        LCD1602_RS_PORT |= LCD1602_RS_MASK; \
-    } while (0)
-
-#define LCD1602_RS_CLR                       \
-    do                                       \
-    {                                        \
-        LCD1602_RS_PORT &= ~LCD1602_RS_MASK; \
-    } while (0)
-
-#define LCD1602_RW_SET                      \
-    do                                      \
-    {                                       \
-        LCD1602_RW_PORT |= LCD1602_RW_MASK; \
-    } while (0)
-
-#define LCD1602_RW_CLR                       \
-    do                                       \
-    {                                        \
-        LCD1602_RW_PORT &= ~LCD1602_RW_MASK; \
-    } while (0)
-*/
-
-/*
-#define LCD1602_E_SET                        \
-    do                                       \
-    {                                        \
-        LCD1602_CTRL_PORT |= LCD1602_E_MASK; \
-    } while (0)
-
-#define LCD1602_E_CLR                         \
-    do                                        \
-    {                                         \
-        LCD1602_CTRL_PORT &= ~LCD1602_E_MASK; \
-    } while (0)
-*/
-
-
-/*
-#define LCD1602_DATA_SET(x)                         \
-    do                                              \
-    {                                               \
-        PORTA = (PORTA & ~LCD1602_DATA_MASK) | (x); \
-    } while (0)
-*/
-
-
 /** LCD driver class, for the typical 16x02 displays
  */
 class Lcd1602Driver
 {
 public:
-    Lcd1602Driver() {}
-    //virtual ~Lcd1602Driver() {}
 
-    //void SetLcdControlBus(uint8_t rs, uint8_t rw, uint8_t e);
-
-    //uint8_t GetBusy(void);
-
-    void WaitForBusy(void);
+    Lcd1602Driver()
+    {
+    }
 
     /** This method sends a pattern to the LCD module using the command
      * interface. If widetrx is set, the lower 4 bits will be sent as
      * a second bus cycle.
      */
-    void SendCmd(uint8_t pattern, uint8_t widetrx);
 
-    void SetBusWidth(uint8_t width);
-    
-    void ClearDisplay(void);
-
-    void ReturnHome(void);
-
-    void EntryMode(uint8_t increment, uint8_t shift);
-
-    void DisplayOn(uint8_t show, uint8_t cursor, uint8_t blink);
-
-    void DisplayShift(uint8_t display, uint8_t right);
-
-    void FunctionSet(uint8_t width, uint8_t dual, uint8_t high);
-
-    void SetAddress(uint8_t cgram, uint8_t addr);
-
-    void SendChar(uint8_t c);
-    
-    void Init(void)
+    void ClearDisplay(void)
     {
-        // Set up the physical bus
-        LCD1602_CTRL_PORT = 0;
-        LCD1602_CTRL_DIR = 0x7f;
-        
-        // Configure the bus interface of the LCD module
-        SetBusWidth(8);
-        SetBusWidth(8);
-        WaitForBusy();
-        SetBusWidth(4);
-        
-        FunctionSet(0, 1, 0);
-        DisplayShift(0, 1);
-        DisplayOn(1, 1, 1);
-        
-        SetAddress(0, 0);
-        SendChar('H');
-        SendChar('e');
-        SendChar('l');
-        SendChar('l');
-        SendChar('o');
+        SendCmd(0x01);
     }
 
-private:
-    //void SetE(void);
-    //void ClearE(void);
-    //void SetDataDir(uint8_t _dir_in);
-    //void SetData(uint8_t v);
+    void ReturnHome(void)
+    {
+        SendCmd(0x02);
+    }
 
+    void EntryMode(uint8_t increment, uint8_t shift)
+    {
+        uint8_t cmd = 0x04 | (increment ? 0x2 : 0) | (shift ? 0x01 : 0);
+        SendCmd(cmd);
+    }
+
+    void DisplayOn(uint8_t show, uint8_t cursor, uint8_t blink)
+    {
+        uint8_t cmd = 0x08 | (show ? 0x04 : 0) | (cursor ? 0x02 : 0) | (blink ? 0x01 : 0);
+        SendCmd(cmd);
+    }
+
+    void DisplayShift(uint8_t display, uint8_t right)
+    {
+        uint8_t cmd = 0x10 | (display ? 0x08 : 0) | (right ? 0x04 : 0);
+        SendCmd(cmd);
+    }
+
+    void FunctionSet(uint8_t width, uint8_t dual, uint8_t high)
+    {
+        uint8_t cmd = 0x20 | (width ? 0x10 : 0) | (dual ? 0x08 : 0) | (high ? 0x04 : 0);
+        SendCmd(cmd);
+    }
+
+    void SetAddress(uint8_t cgram, uint8_t addr)
+    {
+        uint8_t cmd = (cgram ? 0x40 : 0x80) | addr;
+        SendCmd(cmd);
+    }
+
+    void SendChar(uint8_t c);
+
+    void SendSingleCmd(uint8_t pattern);
+
+    void SendCmd(uint8_t pattern);
+
+    void Init(void);
+    
+    void SendStr(const char *s, uint8_t len);
+
+    void SendStr(const char *s);
+
+    void SendConstStr(const char *s, uint8_t len);
+
+    void SendConstStr(const char *s);
+
+private:
+    void SetBusWidth(uint8_t width);
 };
 
 #endif // __LCD_1602_H__
---
+++ src/Lcd1602.cpp
@@ -0,0 +1,191 @@
+/**
+ */
+
+#include "lib.h"
+#include "Lcd1602.h"
+
+#define CMD_8_BITS  1
+#define CMD_4_BITS  0
+#define CMD_DELAY   500
+
+#define LCD_TRX_DATA(p,w)   SendTrx(1, p, w)
+#define LCD_TRX_CMD(p,w)    SendTrx(1, p, w)
+
+static void SetE(uint8_t on) {
+    if (on) {
+        LCD1602_CTRL_PORT |= LCD1602_E_MASK;
+    } else {
+        LCD1602_CTRL_PORT &= ~LCD1602_E_MASK;
+    }
+}
+
+static void SetDataDir(uint8_t _dir_in) {
+    if (_dir_in) {
+        LCD1602_DATA_DIR &= ~LCD1602_DATA_MASK;
+    } else {
+        LCD1602_DATA_DIR |= LCD1602_DATA_MASK;
+    }
+}
+
+static void SetLcdControlBus(uint8_t rs, uint8_t rw) {
+    if (rs) {
+        LCD1602_CTRL_PORT |= LCD1602_RS_MASK;
+    } else {
+        LCD1602_CTRL_PORT &= ~LCD1602_RS_MASK;
+    }
+
+    if (rw) {
+        LCD1602_CTRL_PORT |= LCD1602_RW_MASK;
+    } else {
+        LCD1602_CTRL_PORT &= ~LCD1602_RW_MASK;
+    }
+}
+
+/* ************************************************************************ */
+#pragma GCC push_options
+#pragma GCC optimize ("Os")
+
+static uint8_t SampleBus(void) {
+    uint8_t ret;
+
+    SetDataDir(1);
+    SetE(1);
+    _NOP();
+    ret = LCD1602_DATA_IN;
+    SetE(0);
+    SetDataDir(0);
+
+    return ret;
+}
+
+uint8_t GetData(uint8_t isData) {
+    uint8_t ret;
+
+    if (isData) {
+        // Read data RAM
+        SetLcdControlBus(1, 1);
+    } else {
+        // Read control bus
+        SetLcdControlBus(0, 1);
+    }
+
+    ret = SampleBus() << 4;
+    sleep_us(10);
+    ret |= SampleBus() & 0x0f;
+    SetLcdControlBus(0, 0);
+    return ret;
+}
+
+static void PokeBus(uint8_t d) {
+    LCD1602_DATA_PORT = (LCD1602_DATA_PORT & 0xf0) | (d & 0x0f);
+    SetE(1);
+    _NOP();
+    //sleep_us(1);
+    SetE(0);
+}
+
+#pragma GCC pop_options
+
+/* ************************************************************************ */
+
+uint16_t WaitForBusy(void) {
+    uint8_t count = 0;
+
+    while (GetData(0) & 0x80) {
+        count++;
+    }
+    return count;
+}
+
+void SendTrx(uint8_t pattern, uint8_t doubletrx, uint8_t isData) {
+    WaitForBusy();
+    if (isData) {
+        SetLcdControlBus(1, 0);
+    } else {
+        SetLcdControlBus(0, 0);
+    }
+    PokeBus(pattern >> 4);
+
+    if (doubletrx) {
+        PokeBus(pattern & 0x0f);
+    }
+}
+
+void Lcd1602Driver::SendChar(uint8_t c) {
+    SendTrx(c, 1, 1);
+}
+
+void Lcd1602Driver::SendSingleCmd(uint8_t pattern) {
+    SendTrx(pattern, 0, 0);
+}
+
+void Lcd1602Driver::SendCmd(uint8_t pattern) {
+    SendTrx(pattern, 1, 0);
+}
+
+void Lcd1602Driver::SetBusWidth(uint8_t width) {
+    uint8_t cmd = (width > 4) ? 0x30 : 0x20;
+    SendSingleCmd(cmd);
+}
+
+void Lcd1602Driver::SendStr(const char *s, uint8_t len) {
+    while (len)   {
+        SendChar(*s);
+        --len;
+        ++s;
+    }
+}
+
+void Lcd1602Driver::SendStr(const char *s) {
+    while (1)   {
+        char c = *s;
+        
+        if (0 == c) {
+            break;
+        }
+        SendChar(c);
+        ++s;
+    }
+}
+
+void Lcd1602Driver::SendConstStr(const PROGMEM char *s, uint8_t len) {
+    while (len)   {
+        SendChar(pgm_read_byte(s));
+        --len;
+        ++s;
+    }
+}
+
+void Lcd1602Driver::SendConstStr(const PROGMEM char *s) {
+    while (1) {
+        char c = pgm_read_byte(s);
+        if (0 == c) {
+            break;
+        }
+        SendChar(c);
+        ++s;
+    }
+}
+
+void Lcd1602Driver::Init(void) {
+    uint8_t i;
+
+    // Set up the physical bus
+    LCD1602_CTRL_PORT = 0;
+    LCD1602_CTRL_DIR = 0x7f;
+
+    for (i = 0; i < 5; i++)
+        sleep_us(10000);
+    SetDataDir(0);
+
+    // Configure the bus interface of the LCD module
+    SetBusWidth(8);
+    SetBusWidth(8);
+    SetBusWidth(8);
+    SetBusWidth(4);
+    FunctionSet(0, 1, 0);
+
+    DisplayOn(1, 1, 1);
+    ClearDisplay();
+    EntryMode(1, 0);
+}
